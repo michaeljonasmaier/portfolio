1b0bcc7bf89322fbdda508afff206d248e49fad0
Michael Maier <mm263@hdm-stuttgart.de>
2025-01-21 16:46:09 +0100

    contact section edited

--- src/app/main-content/contact-form/contact-form.component.ts
+++ src/app/main-content/contact-form/contact-form.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { PfBtnComponent } from '../../shared/pf-btn/pf-btn.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [PfBtnComponent],
+  imports: [PfBtnComponent, FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.scss'
 })
 export class ContactFormComponent {
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
 
+  onSubmit() {
+    console.log(this.contactData);
+    var element = <HTMLInputElement>document.getElementById("checkbox");
+    var checkbox = <HTMLInputElement>document.getElementById("errmsg");
+    var isChecked = element.checked;
+    if(!isChecked){
+      checkbox.classList.add("show");
+    } else{
+      checkbox.classList.remove("show");
+    }
+  }
 }
